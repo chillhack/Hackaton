f7c6d91b2b0f255ef8b4ec1176c8a2c5c9d42b1d
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2019-05-18 15:05:50 +0500

    Update Event and Place entities

--- ChillMap/ChillMapWeb/Entities/Event.cs
+++ ChillMap/ChillMapWeb/Entities/Event.cs
@@ -6,5 +6,8 @@ namespace ChillMapWeb.Entities
     {
         public Guid Id { get; private set; }
         public Place Location { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime Date { get; private set; }
     }
 }
