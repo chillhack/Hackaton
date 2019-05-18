e1c84041dd95a1d858f270a62dd3584ddddcd3ab
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2019-05-18 14:15:02 +0500

    Create entities

---
+++ ChillMap/ChillMapWeb/Entities/Place.cs
@@ -0,0 +1,7 @@
+namespace ChillMapWeb.Entities
+{
+    public class Place
+    {
+        
+    }
+}
---
+++ ChillMap/ChillMapWeb/Entities/User.cs
@@ -0,0 +1,7 @@
+namespace ChillMapWeb.Entities
+{
+    public class User
+    {
+        
+    }
+}
